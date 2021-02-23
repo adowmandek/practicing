fc157f8474603dc35997692c12db6a2484e83a82
adowmandek <adowmandek@gmail.com>
2021-02-23 11:17:29 +0300

    first commit

---
+++ src/lopk.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val name:String ="loley"
+    val age=19
+    val weight=34
+    val phoneNumber="0794764520"
+    var kenyanCitizen:Boolean=true
+    println(name)
+    println(age)
+    println(phoneNumber)
+    println(kenyanCitizen)
+}
